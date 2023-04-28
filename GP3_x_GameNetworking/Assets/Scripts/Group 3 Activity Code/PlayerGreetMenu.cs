ba95d66ece9230e38aa58cec37c15f31d2ec6f10
Dustin08M <dustin.macorol@ciit.edu.ph>
2023-04-28 16:17:38 +0800

    Testing to display player's stats on Start

--- GP3_x_GameNetworking/Assets/Scripts/Group 3 Activity Code/PlayerGreetMenu.cs
+++ GP3_x_GameNetworking/Assets/Scripts/Group 3 Activity Code/PlayerGreetMenu.cs
@@ -5,9 +5,15 @@ using UnityEngine.UI;
 
 public class PlayerGreetMenu : MonoBehaviour
 {
+    private PlayerStatsManager _playerstatManager;
     public Text greetingText;
     void Start()
     {
         greetingText.text = "Hello, " + AuthenticationManager2.PlayerName.ToString();
+        _playerstatManager.RetrievePlayerStatsOnNewRun();
+
+        Debug.Log("Distance Traveled: " + _playerstatManager._DistanceTraveleved);
+        Debug.Log("Obstacles Dodged: " + _playerstatManager._ObstaclesDodged);
+        Debug.Log("Coins Collected: " + _playerstatManager._coinsCollected);
     }
 }
